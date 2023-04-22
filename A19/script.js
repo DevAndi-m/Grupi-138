388d9fe99c95e82badd425702d6bdb33e55e7742
Andi Morina <andimorina093@gmail.com>
2023-04-22 14:07:40 +0200

    commiti jone i pare

---
+++ A19/script.js
@@ -0,0 +1,12 @@
+const words = ['apple', 'banana', 'cherry'];
+
+let wordLengths = [];
+
+words.forEach((word) => {
+  let length = word.length;
+  wordLengths.push(length);
+});
+
+console.log(wordLengths);
+
+
